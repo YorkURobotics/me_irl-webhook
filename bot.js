1681ca55791c17987ac1f8427ab73f495685a54b
FrankenMan <epicenigma5@gmail.com>
2017-03-19 16:52:11 +0400

    postlogging working!!!

--- bot.js
+++ bot.js
@@ -11,10 +11,9 @@ client.login(config.bottoken)
 const postDict = JSON.parse(fs.readFileSync('./posts.json', 'utf8'));
 
 function postLog(postId, postUrl) {
-  if (!postDict[postId]) posts[postId] = {
+    postDict[postId] = {
     url: postUrl
   }
-  else{return;}
   fs.writeFile('./posts.json',JSON.stringify(postDict), (err) => {
     if (err) console.error(err)
   })
@@ -35,24 +34,30 @@ request(config.url, function(error,response,body) {
     if (ok.data.url.includes(NUT) && !ok.data.url.includes(ext || otherExt || dril || r34)) {
        const SHACK = ok.data.url + ext
      if (ok.data.url.includes(alb)) return;
-      if (postDict[ok.data.id]) return;
-      else {
-         webhook.sendMessage(`${ok.data.title}\n${SHACK}`);
+      if (!postDict[ok.data.id]){
+        webhook.sendMessage(`${ok.data.title}\n${SHACK}`);
        postLog(ok.data.id, SHACK)
+      }
+      else {
+         return
       }
     }
     else if (ok.data.url.includes("i.reddituploads.com")){
-     if (postDict[ok.data.id]) return;
+     if (!postDict[ok.data.id]) {
+     postLog(ok.data.id,ok.data.preview.images[0].source.url);
+     webhook.sendMessage(`${ok.data.title}\n${ok.data.preview.images[0].source.url}`)
+     }
      else {
-        webhook.sendMessage(`${ok.data.title}\n${ok.data.preview.images[0].source.url}`)
-      postLog(ok.data.id, ok.data.preview.images[0].source.url)
+        return;
      }
     }
     else{
-      if (postDict[ok.data.id]) return;
-      else{
+      if (!postDict[ok.data.id]){
         postLog(ok.data.id, ok.data.url)
         webhook.sendMessage(`${ok.data.title}\n${ok.data.url}`);
+      }
+      else{
+        return;
       }
       
     };
@@ -62,7 +67,7 @@ request(config.url, function(error,response,body) {
 function redditInterval() {
  setInterval(() => (fetchRedditPost()), 360000);
 }
-redditInterval();
+fetchRedditPost();
 
 client.on('ready', () => {
   console.log('Seizing the means of production in 3, 2, 1...');

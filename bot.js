9cce15222dd92811b890d359db3e6f9b1258186b
FrankenMan <epicenigma5@gmail.com>
2017-03-27 20:29:11 +0400

    idk

--- bot.js
+++ bot.js
@@ -71,6 +71,6 @@ function redditInterval() {
 }
 redditInterval();
 
-console.log('Seizing the means of production in 3, 2, 1...');
+console.log('Seizing the means of production in 3, 2, 1...');//jsfd
 
 
